00ab559b6d5bafcacac06767d82ac6e6bf93954e
Awor  Catriona <aworachuil@gmail.com>
2025-04-02 17:36:00 +0300

    commit five
    i have  optimized the exchange rate conversion by removing   white space

--- currency.rates/src/com/rates/ExchangeRateProvider.java
+++ currency.rates/src/com/rates/ExchangeRateProvider.java
@@ -4,12 +4,12 @@ import java.util.Map;
 
 public class ExchangeRateProvider {
     private static final Map<String, Double> rates = new HashMap<>();
-      static {
-            rates.put("USD_TO_EUR", 0.85);
-            rates.put("EUR_TO_USD", 1.18);
-        }
-        public static double getRate(String from, String to) {
-            return rates.getOrDefault(from + "_TO_" + to, -1.0);
-        }
+    static {
+        rates.put("USD_TO_EUR", 0.85);
+        rates.put("EUR_TO_USD", 1.18);
+    }
+    public static double getRate(String from, String to) {
+        return rates.getOrDefault(from + "_TO_" + to, -1.0);
+    }
 
 }
