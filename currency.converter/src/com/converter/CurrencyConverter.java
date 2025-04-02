740a99846ad041e349ae5ee3c185cc16702eb16e
Awor  Catriona <aworachuil@gmail.com>
2025-04-02 14:15:03 +0300

    Commit two
    i  have  corrected  scanner class imports issues and its object that had conflict with Junit testing .

--- currency.converter/src/com/converter/CurrencyConverter.java
+++ currency.converter/src/com/converter/CurrencyConverter.java
@@ -1,14 +1,9 @@
 package com.converter;
 import com.rates.ExchangeRateProvider;
-import java.util.Scanner;
+
 public class CurrencyConverter {
 
     public static double convert(String from, String to, double amount) {
-        Scanner money= new Scanner(System.in);
-        System.out.println("Enter your Amount to change in dollars");
-       // double moneyIndor=money.nextDouble();
-       // moneyIndor =amount;
-
         double rate = ExchangeRateProvider.getRate(from, to);
         if (rate < 0) {
             throw new IllegalArgumentException("Exchange rate not found for " + from + " to " + to);
